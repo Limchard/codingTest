d7e6ee639b8bab99446e6ffc553a333e02988d28
limchard <jksy6458@naver.com>
2023-12-28 14:36:58 +0900

    backjoon 10810 / 성공 / 20분

--- backjoon/A10810.java
+++ backjoon/A10810.java
@@ -5,6 +5,33 @@ public class A10810 {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int m=sc.nextInt();
+        int i,j,k;
+
+        int [] arr=new int[n];
+
+        for (int a=0;a<n;a++){
+            arr[a]=0;
+        }
+
+        for (int a=0;a<m;a++){
+            i=sc.nextInt();
+            j=sc.nextInt();
+            k=sc.nextInt();
+
+            for (int b=i;b<=j;b++){
+                arr[b-1]=k;
+            }
+
+        }
+
+        for (int a=0;a<n;a++){
+            System.out.print(arr[a]+" ");
+        }
+
+
+
+
+
 
 
 
