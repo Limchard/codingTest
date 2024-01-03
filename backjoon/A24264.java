fe909b9843a5b2c12ba5b22982eb3aff44015a37
limchard <jksy6458@naver.com>
2024-01-03 17:36:56 +0900

    [백준] 24264 / 실패 / 20분 : 왜 int가 안될까... 꼭 long을 써야 하나?

--- backjoon/A24264.java
+++ backjoon/A24264.java
@@ -10,8 +10,18 @@ public class A24264 {
 //            return sum;
 //        }
 
+//        1. # 코드 1 은 n의 제곱번 수행했다. i를 n번 실행하고 i가 증가할 때마다 각각 n번 실행했다.
+//        2. 최고 차수는 2이다. n의 제곱이니까
+
         Scanner sc=new Scanner(System.in);
-        
+//        int n=sc.nextInt();
+        long n=sc.nextInt();
+        System.out.println((int)Math.pow(n,2));
+//        System.out.println(n*n);
+        System.out.println(2);
+
+
+
 
     }
 }
