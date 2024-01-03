4d08b2a89d970664221e1d2b489c3c2293513dc1
limchard <jksy6458@naver.com>
2024-01-03 15:30:01 +0900

    [백준] 24263 / 성공 / 5분

---
+++ backjoon/A24263.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class A24263 {
+    public static void main(String[] args) {
+//        MenOfPassion(A[], n) {
+//            sum <- 0;
+//            for i <- 1 to n
+//            sum <- sum + A[i]; # 코드1
+//            return sum;
+//        }
+
+//        1. # 코드 1의 수행 횟수는 n번이다. 왜냐구? for문을 n번 계산 했으니까.
+//        2. 차수는 1이다. n번 실행이니까 n의 1승이다.
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        System.out.println(n);
+        System.out.println(1);
+
+    }
+}
