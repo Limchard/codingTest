5bd8031e8df51f59c18b1b0ad62b295312b632b2
limchard <jksy6458@naver.com>
2024-02-06 12:31:00 +0900

    [백준] 15894 / 성공 / 8분

---
+++ backjoon/A15894.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class A15894 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		long num = sc.nextInt();
+
+		System.out.println(num * 4);
+	}
+}
