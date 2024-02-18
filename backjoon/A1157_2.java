b645c4d8f6bbde584d989887fbfb9d4f66e69a63
limchard <jksy6458@naver.com>
2024-02-18 14:51:29 +0900

    fix : 백준 풀이 수정

--- backjoon/A1157_2.java
+++ backjoon/A1157_2.java
@@ -11,23 +11,28 @@ public class A1157_2 {
 		for (int i = 0; i < input.length(); i++) {
 			if ('A' <= input.charAt(i) && input.charAt(i) <= 'Z') { // 대문자 범위
 				alphabet[input.charAt(i) - 'A']++; // 해당 인덱스의 값 1 증가
+
+				// System.out.println(input.charAt(i)); // 이러면 문자 출력
+				// System.out.println(input.charAt(i) - 'A'); // 이러면 아스키코드가 출력 되는구나...
+
 			} else { // 소문자 범위
 				alphabet[input.charAt(i) - 'a']++;
 			}
 		}
-
 		int max = -1;
-		char ch = '?';
+		char result = '?';
 
 		for (int i = 0; i < 26; i++) {
 			if (alphabet[i] > max) {
 				max = alphabet[i];
-				ch = (char)(i + 65);
+				// System.out.println("ch1: " + ch); // ? 표 출력.. 왜?
+				result = (char)(i + 65);
+				// System.out.println("ch2: " + ch); // 문자 출력; 위에서 타입을 char 로 지정했으니까
 			} else if (alphabet[i] == max) {
-				ch = '?';
+				result = '?';
 			}
 		}
-		System.out.println(ch);
+		System.out.println(result);
 
 	}
 }
