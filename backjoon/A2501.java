75b3ce96f6f4c32a59a97ff3a6bcaa339bbd871e
limchard <jksy6458@naver.com>
2024-02-19 15:39:01 +0900

    [백준] 2501 / 20분 / 성공

---
+++ backjoon/A2501.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class A2501 {
+	public static void main(String[] args) {
+		// 입력숫자 2개
+		// 약수를 저장할 배열
+		// 	나눠서 0이 되면 해당 값 배열에 할당
+		// 	해당 번지수 출력
+
+		Scanner sc = new Scanner(System.in);
+		int naturalNumber = sc.nextInt();
+		int orderNumber = sc.nextInt();
+		int divisor[] = new int[orderNumber]; // 3 -> 0,1,2
+		int arrayCount = 0; // 배열 번지수 number;
+
+		for (int i = 1; i <= naturalNumber; i++) {
+			if (naturalNumber % i == 0) {
+				divisor[arrayCount] = i;
+				arrayCount++;
+			} else if (divisor[orderNumber - 1] != 0) { // 2까지 차면 break;
+				break;
+			}
+
+		}
+
+		if (divisor[orderNumber - 1] == 0) {
+			System.out.println(0);
+		} else {
+			System.out.println(divisor[orderNumber - 1]);
+		}
+
+	}
+}
