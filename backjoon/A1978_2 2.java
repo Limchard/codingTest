a63df33dfd97ca24b5ce228b799db659a5b1672c
limchard <jksy6458@naver.com>
2025-02-03 22:17:24 +0900

    백준 / 2605 / 성공 / 1시간

--- backjoon/A1978_2 2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class A1978_2
-{
-    public static void main(String[] args)
-    {
-
-    }
-}
--- backjoon/A2605.java
+++ backjoon/A2605.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
 
 public class A2605 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+		int n = sc.nextInt();
 
-        int[] array = new int[n];
+		int[] array = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            array[i] = i + 1;
-        }
+		for (int i = 0; i < n; i++) {
+			array[i] = i + 1;
+		}
 
-        for (int i = 0; i < n; i++) {
-            int r = sc.nextInt();
-            if (r == 0) {
-                continue;
-            }
-            for (int j = 1; j <= r; j++) {
-                int temp = array[i - j];
-                array[i - j] = array[i];
-                array[i] = temp;
-            }
-        }
+		for (int i = 0; i < n; i++) {
+			int r = sc.nextInt(); // r은 i 보다 작거나 같은 자연수
+			if (r == 0) {
+				continue;
+			}
 
-        for (int number : array) {
-            System.out.print(number + " ");
-        }
-    }
+			for (int j = 1; j <= r; j++) {
+				int temp = array[i - j];
+				array[i - j] = array[i - j + 1];
+				array[i - j + 1] = temp;
+			}
+		}
+
+		for (int number : array) {
+			System.out.print(number + " ");
+		}
+	}
 }
 
